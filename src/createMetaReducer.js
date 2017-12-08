7b072fa11654fa5d7192f6dd2a52620c36334d1b
Nastasia <muaaaaaaa@mail.ru>
2017-12-08 11:32:59 +0300

    add takeOnce, add last update date in meta (#9)

--- src/createMetaReducer.js
+++ src/createMetaReducer.js
@@ -2,14 +2,15 @@ import { toSuccess, toReset, toError, toRequest } from './actionHelpers';
 import { getPath, noParamsKey, defaultKey } from './asyncHelpers';
 import { assoc, pathOr } from 'ramda';
 
-const defaultState = { pending: false, error: false, success: false };
+const defaultState = { pending: false, error: false, success: false, lastSucceedAt: null };
 
 export default function createMetaReducer(actionName) {
     return (state = { [defaultKey]: defaultState }, { type, payload, params, requestAction }) => {
         const param = pathOr(params || noParamsKey, ['params'], requestAction);
+        const getLastSucceedAt = () => pathOr(null, [getPath(param), 'lastSucceedAt'], state);
 
-        const getState = (pending, error, success) => {
-            const meta = { pending, error, success };
+        const getState = (pending, error, success, lastSucceedAt = getLastSucceedAt()) => {
+            const meta = { pending, error, success, lastSucceedAt };
             return assoc(getPath(param), meta, state);
         };
 
@@ -17,11 +18,11 @@ export default function createMetaReducer(actionName) {
             case toRequest(actionName):
                 return getState(true, false, false);
             case toSuccess(actionName):
-                return getState(false, false, true);
+                return getState(false, false, true, (new Date).toISOString());
             case toError(actionName):
                 return getState(false, payload, false);
             case toReset(actionName):
-                return getState(false, false, false);
+                return getState(false, false, false, null);
             default:
                 return state;
         }
--- src/utils/saga.js
+++ src/utils/saga.js
@@ -1,5 +1,6 @@
-import { take, fork, join } from 'redux-saga/effects';
-import { append, contains, not, prop, without } from 'ramda';
+import { take, fork, join, select } from 'redux-saga/effects';
+import { append, contains, not, prop, without, path } from 'ramda';
+import { noParamsKey, getPath } from '../asyncHelpers';
 
 export const createTakeFirst = storedBy => function*(pattern, saga, ...args) {
     const task = yield fork(function*() {
@@ -16,6 +17,38 @@ export const createTakeFirst = storedBy => function*(pattern, saga, ...args) {
                 yield fork(function*() {
                     yield join(firstTask);
                     takedParams = without([params], takedParams);
+                });
+            }
+        }
+        /* eslint-enable no-loop-func */
+    });
+
+    return task;
+}
+
+export function* takeOnce(pattern, saga, ...args) {
+    const task = yield fork(function*() {
+        let onceTasks = [];
+        /* eslint-disable no-loop-func */
+        while (true) {
+            const action = yield take(pattern);
+            const params = prop('params', action);
+            const pathName = getPath(params || noParamsKey);
+
+            if (not(contains(params, onceTasks))) {
+                const success = yield select(state => path(['asyncClient', 'meta', pattern, pathName, 'success'], state));
+                onceTasks = append(params, onceTasks);
+
+                if (success) {
+                    return;
+                }
+
+                const onceTask = yield fork(saga, ...args.concat(action));
+                yield fork(function*() {
+                    const { error } = yield join(onceTask);
+                    if (error) {
+                        onceTasks = without([params], onceTasks);
+                    }
                 });
             }
         }
